c80c0f98c10e89a5e694f7c43f358fe8dec477e8
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-07-31 00:47:25 +0200

    Connect to websocket

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 // Import libraries and create a discord client
 const Discord = require('discord.js');
-const io = require('socket.io');
+const io = require('socket.io-client');
 const fs = require('fs');
+const execFile = require('child_process').execFile;
 
 let client = new Discord.Client();
-var socket;
 
 const config = JSON.parse(fs.readFileSync('config.json')) // Import the config
+
+if (config.launch_server) {
+    console.log('Launching websocket server!')
+    try {
+        const child = execFile(config.server_path, (err) => {
+            if (err) throw err;
+        });
+        console.log('Success! Websocket server was started');
+    } catch {
+        console.log('Error! Failed to launch server!')
+    }
+}
+
+var socket = io(config.server_url);
+socket.on('connect', () => {
+    const auth = {
+        "auth":config.auth_token,
+        "name": config.client_name,
+        "type": config.client_type
+    };
+    socket.send(JSON.stringify(auth));
+});
 
 client.login(config.token); // Login to the bot account
 
@@ -16,25 +38,6 @@ client.on('ready', async () => {
         type: "WATCHING"
     });
     console.log('Bot is up and running!');
-    if (config.launch_server) {
-        console.log('Launching websocket server!')
-        try {
-            // Launch server
-            setTimeout(10)
-            console.log('Success! Websocket server was started');
-        } catch {
-            console.log('Error! Failed to launch server!')
-        }
-
-
-    }
-    try {
-        socket = io.connect(config.server_url);
-    } catch {
-        chatbridge_enabled = false;
-        console.log('Error! Websocket is not running! Stopping client');
-        process.exit();
-    }
 })
 
 // Send data to websocket when message is received in chatbridge channel
@@ -48,11 +51,10 @@ client.on('message', async message => {
     }
 })
 
-/*
 // Send message to channel if message is recieved
-socket.on('message', function (data) {
+socket.on('message', function(data) {
     const msg = JSON.parse(data);
     if (msg.msg != null) {
         client.guilds.channels.cache.get(config.channel_id).send(`[${msg.client_name}] <${msg.username}> ${msg.msg}`);
     };
-}) */
+});
